81463d84636f309d54256af1c3943be40122d4b4
mjaumi <mjaumi2864@gmail.com>
2025-02-09 21:49:52 +0600

    delete task functionality added

--- script.js
+++ script.js
@@ -6,29 +6,23 @@ function loadTasks() {
 }
 
 // function to add tasks to DOM declared here
-function addTaskToDOM(taskText) {
+function addTaskToDOM(task) {
     const taskList = document.getElementById('task-list')
 
     let li = document.createElement('li');
 
     li.innerHTML = `
-        <span>${taskText}</span>
         <span>
-            <span class="edit" onclick="editTask(this)" >Edit</span>
-            <span class="delete" onclick="deleteTask(this)" >Delete</span>
+            <span class="task-id">${task.id}.</span>
+            <span class="task-text">${task.text}</span>
+        </span>
+        <span>
+            <span class="edit" onclick="editTask(this)">Edit</span>
+            <span class="delete" onclick="deleteTask(this)">Delete</span>
         </span>
     `
 
     taskList.appendChild(li)
-}
-
-// function to save tasks to local storage declared here
-function saveTasksToLocalStorage(newTask) {
-    const taskList = JSON.parse(localStorage.getItem('task-list')) || []
-
-    taskList.push(newTask);
-
-    localStorage.setItem('task-list', JSON.stringify(taskList))
 }
 
 // function to add new task declared here
@@ -40,10 +34,30 @@ function addNewTask() {
         return
     }
 
-    addTaskToDOM(taskInput.value)
-    saveTasksToLocalStorage(taskInput.value)
+    const taskList = JSON.parse(localStorage.getItem('task-list')) || []
+
+    const newTask = { id: taskList.length + 1, text: taskInput.value }
+
+    addTaskToDOM(newTask)
+
+    taskList.push(newTask)
+    localStorage.setItem('task-list', JSON.stringify(taskList))
 
     taskInput.value = ''
+}
+
+// function to delete task from list declared here
+function deleteTask(element) {
+    const taskTextElement = element.parentElement.parentElement
+
+    const taskId = taskTextElement.firstElementChild.firstElementChild.innerText.split('.')[0]
+
+    taskTextElement.remove()
+
+    // saving updated task into local storage here
+    let taskList = JSON.parse(localStorage.getItem('task-list')) || []
+    taskList = taskList.filter(task => task.id !== Number(taskId))
+    localStorage.setItem('task-list', JSON.stringify(taskList))
 }
 
 loadTasks()
